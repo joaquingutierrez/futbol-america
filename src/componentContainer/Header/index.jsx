7532627ab4b1340b911e7e85a4c27ad1cdaf1a86
joaquingutierrez <joaquinguty@gmail.com>
2024-06-07 21:59:08 -0300

    Fix: Solucionado error con position sticky y el menu hamburguesa

--- src/componentContainer/Header/index.jsx
+++ src/componentContainer/Header/index.jsx
@@ -8,14 +8,14 @@ const Header = ({ links }) => {
     return (
         <header className="header">
             <div className="header-content">
-            <div>
-                <h1 className="title"><Link to="/"><span className="yellowColor">FUTBOL</span> AMERICA</Link></h1>
-            </div>
-            <div className="navOptions">
-                <DropDown />
-                <NavBar links={links} />
-                <HamburguerMenu links={links}/>
-            </div>
+                <div>
+                    <h1 className="title"><Link to="/"><span className="yellowColor">FUTBOL</span> AMERICA</Link></h1>
+                </div>
+                <div className="navOptions">
+                    <DropDown />
+                    <NavBar links={links} />
+                    <HamburguerMenu links={links} />
+                </div>
             </div>
         </header>
     )
