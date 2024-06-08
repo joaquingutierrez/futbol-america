1d174ea94182ccc81649624ccab9181da12bf2bb
joaquingutierrez <joaquinguty@gmail.com>
2024-06-07 21:13:59 -0300

    Feature: Schedules para Brazil y Argenina

--- src/components/Schedules/index.jsx
+++ src/components/Schedules/index.jsx
@@ -1,6 +1,6 @@
 import "./style.css"
 
-const Schedules = () => {
+const Schedules = ({schedule}) => {
 
     return (
         <div className="SchedulesContainer">
@@ -9,37 +9,37 @@ const Schedules = () => {
                 <p>Airport</p>
                 <p>Check In Hotel</p>
                 <p>Lunch</p>
-                <p>Free</p>
+                <p>{schedule === "Brazil" ? "Training" : "Free" }</p>
                 <p>Dinner</p>
             </div>
             <div>
                 <h3>DAY 2</h3>
                 <p>Breakfast</p>
-                <p>Training</p>
+                <p>{schedule === "Brazil" ? "Game" : "Training" }</p>
                 <p>Lunch</p>
-                <p>Training</p>
+                <p>{schedule === "Brazil" ? "Walking Tour" : "Training" }</p>
                 <p>Dinner</p>
             </div>
             <div>
                 <h3>DAY 3</h3>
                 <p>Breakfast</p>
-                <p>Game</p>
+                <p>{schedule === "Brazil" ? "Training" : "Game" }</p>
                 <p>Lunch</p>
-                <p>Walking Tour</p>
+                <p>{schedule === "Brazil" ? "Training" : "Walking Tour" }</p>
                 <p>Dinner</p>
             </div>
             <div>
                 <h3>DAY 4</h3>
                 <p>Breakfast</p>
-                <p>Training</p>
+                <p>{schedule === "Brazil" ? "Game" : "Training" }</p>
                 <p>Lunch</p>
-                <p>Museum</p>
+                <p>{schedule === "Brazil" ? "Tour Corcovado" : "Museum" }</p>
                 <p>Dinner</p>
             </div>
             <div>
                 <h3>DAY 5</h3>
                 <p>Breakfast</p>
-                <p>Game</p>
+                <p>{schedule === "Brazil" ? "Training" : "Game" }</p>
                 <p>Lunch</p>
                 <p>Training</p>
                 <p>Dinner</p>
@@ -47,7 +47,7 @@ const Schedules = () => {
             <div>
                 <h3>DAY 6</h3>
                 <p>Breakfast</p>
-                <p>Training</p>
+                <p>{schedule === "Brazil" ? "Game" : "Training" }</p>
                 <p>Lunch</p>
                 <p>Pool</p>
                 <p>Dinner</p>
@@ -55,7 +55,7 @@ const Schedules = () => {
             <div>
                 <h3>DAY 7</h3>
                 <p>Breakfast</p>
-                <p>Game</p>
+                <p>{schedule === "Brazil" ? "Beach Training" : "Game" }</p>
                 <p>Lunch</p>
                 <p>1st Division Game</p>
                 <p>Dinner</p>
@@ -65,7 +65,7 @@ const Schedules = () => {
                 <p>Breakfast</p>
                 <p>Check Out Hotel</p>
                 <p>Lunch</p>
-                <p>Tour Buenos Aires</p>
+                <p>{schedule === "Brazil" ? "Tour Rio de Janeiro" : "Tour Buenos Aires" }</p>
                 <p>Airport</p>
             </div>
         </div>
