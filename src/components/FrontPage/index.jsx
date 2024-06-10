0ae69f420e84dbbd6b9986357548d5475fc42eb6
joaquingutierrez <joaquinguty@gmail.com>
2024-06-10 09:35:05 -0300

    Segundo intento para arreglar el video en safari

--- src/components/FrontPage/index.jsx
+++ src/components/FrontPage/index.jsx
@@ -1,20 +1,19 @@
 import "./style.css"
 import videoFrontPage from "../../assets/videos/home-frontPage.mp4"
 import Button from "../Button"
+import { useEffect, useRef } from "react"
 
 const FrontPage = () => {
 
-    document.addEventListener('DOMContentLoaded', (event) => {
-        const video = document.getElementById('contactVideo');
-        video.muted = true; // Asegúrate de que el video esté silenciado
-        video.play().catch((error) => {
-            console.error('Error attempting to play video:', error);
-        });
-    });
+    const videoRef = useRef(null)
+
+useEffect(()=> {
+    videoRef.current.play()
+},[])
 
     return (
         <div className="home-frontPage">
-            <video autoPlay muted loop playsinline id="fontPageVideo">
+            <video ref={videoRef} width="100%" height="100%" autoplay="autoplay" muted loop playsinline id="fontPageVideo">
                 <source src={videoFrontPage} type="video/mp4" />
             </video>
             <div className="home-frontPage-container">
