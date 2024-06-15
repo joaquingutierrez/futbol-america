b14b6b5776a4ee1d25d651e7c0d7c2792d4dca7c
joaquingutierrez <joaquinguty@gmail.com>
2024-06-15 09:10:47 -0300

    Feature: Formulario de contacto funcionando

--- src/componentContainer/Contact/index.jsx
+++ src/componentContainer/Contact/index.jsx
@@ -1,24 +1,52 @@
+import { useRef } from "react"
+import emailjs from '@emailjs/browser';
+import Swal from 'sweetalert2'
+
 import "./style.css"
 import video from "../../assets/videos/contact-video.mp4"
 import Button from "../../components/Button"
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = () => {
+        emailjs
+            .sendForm('service_w1crit9', 'template_djk128h', form.current, {
+                publicKey: 'Ts2VPzmMGRbcl2hXY',
+            })
+            .then(
+                () => {
+                    Swal.fire({
+                        title: "Your email was sent successfully!",
+                        icon: "success"
+                    });
+                    form.current.reset()
+                },
+                (error) => {
+                    Swal.fire({
+                        title: "Sorry, something went wrong!",
+                        text: "Error: " + error,
+                        icon: "error"
+                    });
+                },
+            );
+    };
     return (
         <>
             <main className="contactMain">
                 <div className="contactContent">
                     <h2><span className="yellowColor">YOUR ADVENTURE</span> IS JUST A STEP AWAY</h2>
                     <h3>Contact us and live the experience.</h3>
-                    <form className="contactForm">
+                    <form ref={form} className="contactForm">
                         <input className="contact-textInput" type="text" name="fullName" id="fullName" placeholder="Full Name" />
                         <input className="contact-textInput" type="email" name="email" id="email" placeholder="Email Adress" />
                         <textarea className="contact-textAreaInput" name="textArea" id="textArea" placeholder="Message"></textarea>
                         <div className="contact-checkboxContainer">
-                            <input type="checkbox" name="" id="" />
+                            <input type="checkbox" name="sendInformation" />
                             <p>By submitting this form you agree to send your informationto Futbol America . We will use it
                                 to support our commercial activity and send your newsletters according to our pricavy policy.</p>
                         </div>
-                        <Button title="Submit" white={false} />
+                        <Button handleClick={sendEmail} title="Submit" white={false} />
                     </form>
                 </div>
             </main>
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom"
 import "./style.css"
 
-const Button = ({ title, white, toLink }) => {
+const Button = ({ title, white, toLink, handleClick }) => {
 
     const handleLinkClick = () => {
         window.scrollTo(0, 0);
     };
 
     return (
-        <Link to={toLink} onClick={handleLinkClick} className="buttonLink">
+        <Link to={toLink} onClick={handleClick ? handleClick : handleLinkClick} className="buttonLink">
             <button className={`button ${white ? "button-white" : "button-yellow"}`}>
                 {title}
             </button>
