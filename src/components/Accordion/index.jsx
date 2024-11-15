52515a7ada32964e7afbceadcb18ee599ec5536f
joaquingutierrez <joaquinguty@gmail.com>
2024-11-15 13:31:41 -0300

    Cambiada orientacion del acordeion

--- src/components/Accordion/index.jsx
+++ src/components/Accordion/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { useRef, useState } from "react"
 
 import "./style.css"
 import AccordionMenu from "../AccordionMenu";
@@ -9,10 +9,8 @@ const Accordion = ({ items }) => {
     const [someOtherExpanded, setSomeOtherExpanded] = useState(false);
     const [widthAcc, setWidthAcc] = useState(0)
     const itemRefs = useRef([]);
-    const accordionRef = useRef(null)
 
     const handleClick = (index) => {
-        console.log(widthAcc)
         setExpandedIndex((prev) => {
             if (prev === index) {
                 setSomeOtherExpanded(false)
@@ -24,16 +22,6 @@ const Accordion = ({ items }) => {
 
     }
 
-    useEffect(() => {
-        setWidthAcc(accordionRef.current.offsetWidth)
-    }, [])
-
-    const calcPosition = (index) => {
-        const place = widthAcc / 2
-        return place - ((11 - index) * 80)
-    }
-
-
     return (
         <section className="accordion-container">
             <div className="accordion-text">
@@ -41,28 +29,28 @@ const Accordion = ({ items }) => {
                 <p>Step into a world where soccer transcends boundaries and becomes a transformative journey of discovery and achievement. We're more than just a destination – we're your gateway to a realm where passion meets purpose, where players, coaches, and fans alike unite in the pursuit of greatness. With a steadfast commitment to excellence and innovation, we're here to empower you to unleash your full potential on and off the field. Our holistic approach to soccer development goes beyond the game itself, encompassing personalized mentorship, immersive experiences, and sustainable practices that shape the future of the sport.</p>
             </div>
             <div className="accordion-contentContainer">
-                <div ref={accordionRef} className={`accordion-content ${someOtherExpanded ? "accordion-content-move" : ""}`}>
-                    {items.map((item, index) => {
-                        return (
-                            <div key={index} ref={el => itemRefs.current[index] = el} className={`accordion-item ${expandedIndex === index ? "accordion-item-expand" : ""} ${(expandedIndex !== index && someOtherExpanded) ? "accordion-item-disappear" : ""}`} style={{ zIndex: index, left: `${calcPosition(index)}px` }}>
-                                <div className="accordion-item-sideTitle">
-                                    <div className="accordion-item-expandButtonContainer" onClick={() => handleClick(index)}>
-                                        <div className={`accordion-item-expandButton-verticalLine ${expandedIndex === index ? "accordion-item-expandButton-verticalLineisExpanded" : ""}`}></div>
-                                        <div className="accordion-item-expandButtonHotizontalLine"></div>
-                                    </div>
-                                    <h4>{item.sideTitle}</h4>
-                                </div>
-                                <div className="accordion-item-imgContainer">
-                                    <img loading="lazy" src={item.img} alt="" />
-                                </div>
-                                <div className="accordion-item-textContainer">
-                                    <h3>{item.title}</h3>
-                                    <p>{item.text}</p>
-                                </div>
+                {items.map((item, index) => (
+                    <>
+                        <div key={index} className={`accordion-itemContainer ${expandedIndex == index ? "accordion-itemContainer-expand" : ""}`}>
+                            <div className="accordion-item-sideContainer">
+                                <h4>{item.sideTitle}</h4>
+                                <button className="accordion-sideItem-ButtonContainer" onClick={() => handleClick(index)}>
+                                    <div className={`accordion-sideItem-ButtonContainer-verticalLine ${expandedIndex == index ? "accordion-sideItem-ButtonContainer-verticalLine-expanded" : ""}`}></div>
+                                    <div className="accordion-sideItem-ButtonContainer-horizontalLine"></div>
+                                </button>
                             </div>
-                        )
-                    })}
-                </div>
+                        </div>
+                        <div className={`accordion-item-extraContentContainer ${expandedIndex == index ? "accordion-itemContainer-expand" : ""}`}>
+                            <div className="accordion-item-extraContent-imgContainer">
+                                <img src={item.img} alt="" />
+                            </div>
+                            <div className="accordion-item-extraContent-textContainer">
+                                <h3>{item.title}</h3>
+                                <p>{item.text}</p>
+                            </div>
+                        </div>
+                    </>
+                ))}
             </div>
             <div className="accordion-phone-contentContainer">
                 <AccordionMenu items={items} />
