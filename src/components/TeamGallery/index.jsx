a5addfb7a23eea432c08d4b7fa911ee6c7ee4050
joaquingutierrez <joaquinguty@gmail.com>
2024-11-16 10:22:15 -0300

    Cambiada el layout de teamGallery

--- src/components/TeamGallery/index.jsx
+++ src/components/TeamGallery/index.jsx
@@ -1,49 +1,26 @@
-import { useState } from "react"
-
 import "./style.css"
 
 const TeamGallery = ({ team }) => {
 
-    const [currentIndex, setCurrentIndex] = useState(0)
-    const [horizontalAnimation, setHorizontalAnimation] = useState("")
-    const [verticalAnimation, setVerticalAnimation] = useState("")
-
-
-
-    const handleClickPrev = () => {
-        if (currentIndex <= 0) return
-        setVerticalAnimation("slideInUp")
-        setHorizontalAnimation("slideInLeft")
-        setCurrentIndex(prev => {
-            return prev - 1
-        })
-    }
-    const handleClickNext = () => {
-        if (currentIndex >= team.length - 1) return
-        setVerticalAnimation("slideInDown")
-        setHorizontalAnimation("slideInRight")
-        setCurrentIndex(prev => {
-            return prev + 1
-        })
-    }
-
     return (
-        <div className="teamGallery-container">
-            <div className="teamGallery-imageContainer">
-                <img loading="lazy" src={team[currentIndex].img} alt="" className={horizontalAnimation} onAnimationEnd={() => setHorizontalAnimation("")} />
-            </div>
-            <div className="teamGallery-content">
-                <div className="teamGallery-infoContainer">
-                    <h3 className={`teamGallery-name ${verticalAnimation}`} onAnimationEnd={() => setVerticalAnimation("")}>{team[currentIndex].name}</h3>
-                    <p className={`teamGallery-desc ${verticalAnimation}`} onAnimationEnd={() => setVerticalAnimation("")}>{team[currentIndex].desc}</p>
-                    {team[currentIndex].contactMail ? <p className={`teamGallery-desc ${verticalAnimation}`} onAnimationEnd={() => setVerticalAnimation("")}>Contact Email: {team[currentIndex].contactMail}</p> : <p></p>}
-                </div>
-                <div className="teamGallery-buttons">
-                    {currentIndex > 0 && <p className="teamGallery-buttonPrev"><span onClick={handleClickPrev}>←</span></p>}
-                    {currentIndex < team.length - 1 && <p className="teamGallery-buttonNext"><span onClick={handleClickNext}>→</span></p>}
-                </div>
-            </div>
-        </div>
+        <section className="teamGallery-container">
+            {team.map((item, index) => {
+                return (
+                    <div key={index} className={`teamGallery-cardContainer ${index % 2 === 0 ? "teamGallery-leftCardContainer" : "teamGallery-rightCardContainer"}`}>
+                        {index === 0 && <div className="teamGallery-topSeparator teamGallery-separator"></div>}
+                        <div className={`teamGallery-imgContainer`}>
+                            <img src={item.img} alt="" />
+                        </div>
+                        <h3 className={`teamGallery-name`}>{item.name}</h3>
+                        <p className={`teamGallery-desc`}>{item.desc}</p>
+                        <div className={`teamGallery-mailContainer`}>
+                            {item.contactMail && <a href={`mailto: ${item.contactMail}`}>{item.contactMail}</a>}
+                        </div>
+                        <div className="teamGallery-separator"></div>
+                    </div>
+                )
+            })}
+        </section>
     )
 }
 
